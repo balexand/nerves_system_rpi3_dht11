1b55ab2182c2fcac027fd17d0080f07f4f6a3aa4
Brian Alexander <balexand@gmail.com>
2018-03-10 12:47:20 -0700

    artifact_sites config

--- mix.exs
+++ mix.exs
@@ -34,9 +34,9 @@ defmodule NervesSystemRpi3.Mixfile do
   def nerves_package do
     [
       type: :system,
-      # artifact_sites: [
-      #   {:github_releases, "nerves-project/#{@app}"}
-      # ],
+      artifact_sites: [
+        {:github_releases, "balexand/#{@app}"}
+      ],
       platform: Nerves.System.BR,
       platform_config: [
         defconfig: "nerves_defconfig"
